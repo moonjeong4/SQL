1f1d4b45a04c499aada139b85a371e0038ad3dad
MOON <94416728+moonjeong4@users.noreply.github.com>
2023-03-25 01:32:38 -0400

    Exercises
    • Uses 2 tables; a table of adjectives and a table of nouns
      • Import from text files provided, or use online source
      • 2 columns each
    • Create a stored procedure that adds n band names to a table
      • Generates names by joining two random words together
        • Adjective, then noun
        • Generate the name
          • If the noun doesn't end with "s" - add an "s" (primitive pluralization)
          • Join the two words together
        • Insert into a (3rd) table, band
        • Loop n times

---
+++ J04lab4.sql
@@ -0,0 +1,56 @@
+delimiter //
+
+CREATE or REPLACE PROCEDURE generateNames(in n int UNSIGNED)
+BEGIN 
+			
+
+	loopy: loop
+		if(n = 0) then
+			leave loopy;
+		end if;
+	
+		SELECT adjective.adjective into @ad from adjective order by rand() LIMIT 1;
+	    SELECT noun.noun into @nn from noun order by rand() LIMIT 1;
+	    set @bandName := CONCAT(@ad," ",@nn);
+	   
+	    if right(@nn,1) <> "s" then
+
+	    	set @bandName := CONCAT(@bandName,"s");
+	   
+	    end if;
+	   
+		INSERT into `band`(`name`) values (@bandName);
+		
+		set n := n -1;
+	end loop;
+
+end//
+
+delimiter ;
+
+
+call generateNames(1);
+
+
+
+
+nouns.txt
+drawer
+circle
+line
+office
+duck
+pet
+fall
+creator
+
+
+adjectives.txt
+fast
+responsible
+abiding
+wonderful
+supreme
+waggish
+elite
+boorish
